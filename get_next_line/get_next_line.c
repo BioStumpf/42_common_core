4e2f29205c1862a9d15ebb4a4ec03894d1a9df01
David Stumpf <dstumpf@c2r3p10.42vienna.com>
2025-10-26 11:02:49 +0100

    transformed funcs

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,24 +6,22 @@
 /*   By: dstumpf <dstumpf@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 15:03:48 by dstumpf           #+#    #+#             */
-/*   Updated: 2025/10/25 17:39:47 by dstumpf          ###   ########.fr       */
+/*   Updated: 2025/10/26 11:00:56 by dstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char *ft_realloc(t_fdlist *stash, size_t new_len)
+static char	*ft_realloc(t_fdlist *stash, size_t new_len)
 {
 	char	*new_line;
 	size_t	i;
 
-	if (!stash->line || new_len == 1 || stash->buff_s < 0)
-		return (NULL);
 	new_line = malloc(new_len);
-	if (!new_line)
+	if (!new_line || !stash->line || !stash->line_i || stash->buff_s < 0)
 	{
+		free(new_line);
 		free(stash->line);
-		stash->line = NULL;
 		return (NULL);
 	}
 	i = 0;
@@ -35,66 +33,46 @@ static char *ft_realloc(t_fdlist *stash, size_t new_len)
 	new_line[i] = '\0';
 	stash->line_s = new_len;
 	free(stash->line);
-	stash->line = NULL;
 	return (new_line);
 }
 
-static void	free_stash(t_fdlist **stash)
+void	init_stash(t_fdlist *stash)
 {
-	if ((*stash)->line)
-	{
-		free((*stash)->line);
-		(*stash)->line = NULL;
-	}
-	free(*stash);
-	*stash = NULL;
-}
-
-static t_fdlist	*handle_stash(t_fdlist *stash)
-{
-	if (!stash)
-	{
-		stash = malloc(sizeof(t_fdlist));
-		if (!stash)
-			return (NULL);
-		stash->buff_i = 1;
-		stash->buff_s = 1;
-		(stash->buff)[0] = '\0';
-	}
-	stash->line = malloc(BUFFER_SIZE);
 	stash->line_s = BUFFER_SIZE;
 	stash->line_i = 0;
-	if (stash->buff_s <= 0)
-		free_stash(&stash);
-	return (stash);
+	stash->line = malloc(BUFFER_SIZE * sizeof(char));
 }
 
-char	*get_next_line(int fd)
+void	cpy_buff_to_line(t_fdlist *stash, int fd)
 {
-	static t_fdlist	*stash;
-	char			*out;
-
-	if (BUFFER_SIZE < 0 || fd < 0)
-		return (NULL);
-	stash = handle_stash(stash);
-	if (!stash)
-		return (NULL);
-	while (stash->line && stash->buff_s > 0)
+	while (stash->line)
 	{
 		if (stash->buff_i >= stash->buff_s)
 		{
 			stash->buff_i = 0;
 			stash->buff_s = read(fd, stash->buff, BUFFER_SIZE);
 		}
+		if (!stash->buff_s)
+			break ;
 		if (stash->line_i >= stash->line_s)
 			stash->line = ft_realloc(stash, stash->line_s * 2);
+		if (!stash->line)
+			break ;
 		if (stash->buff_i < stash->buff_s && stash->line)
 			(stash->line)[(stash->line_i)++] = (stash->buff)[stash->buff_i];
 		if ((stash->buff)[(stash->buff_i)++] == '\n')
 			break ;
 	}
-	out = ft_realloc(stash, stash->line_i + 1);
-	if (!out)
-		free_stash(&stash);
-	return (out);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_fdlist	stash;
+
+	if (BUFFER_SIZE <= 0 || fd < 0)
+		return (NULL);
+	init_stash(&stash);
+	cpy_buff_to_line(&stash, fd);
+	stash.line = ft_realloc(&stash, stash.line_i + 1);
+	return (stash.line);
 }
