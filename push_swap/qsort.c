31298d9646239428cc0ff8203af16faf04f1a0b6
David Stumpf <dstumpf@c3r1p5.42vienna.com>
2026-02-17 19:25:26 +0100

    add conditions for odd and even numbers to qsort

--- push_swap/qsort.c
+++ push_swap/qsort.c
@@ -6,27 +6,30 @@
 /*   By: dstumpf <dstumpf@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 09:25:57 by dstumpf           #+#    #+#             */
-/*   Updated: 2026/02/07 15:41:08 by dstumpf          ###   ########.fr       */
+/*   Updated: 2026/02/17 19:00:17 by dstumpf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	sa_to_sb(t_data *dat, size_t mid)
+static void	sa_to_sb(t_data *dat, size_t mid, size_t *pushed)
 {
 	if (dat->sa->head->indx < mid)
+	{
 		push(dat->sa, dat->sb, 'b');
+		*pushed += 1;
+	}
 	else
 		rotate(dat->sa, 'a');
 }
 
 static void	split_halves(t_data *dat, size_t range, size_t mid)
 {
-	size_t	i;
+	size_t	pushed;
 
-	i = 0;
-	while (i++ < range)
-		dat->f(dat, mid);
+	pushed = 0;
+	while (pushed < range / 2)
+		dat->f(dat, mid, &pushed);
 }
 
 void	qsort_a(t_data *dat, size_t range, size_t mid)
@@ -38,7 +41,10 @@ void	qsort_a(t_data *dat, size_t range, size_t mid)
 	}
 	dat->f = sa_to_sb;
 	split_halves(dat, range, mid);
-	qsort_a(dat, range / 2, mid + mid / 2);
+	if (range % 2)
+		qsort_a(dat, range / 2 + 1, mid + mid / 2 + 1);
+	else
+		qsort_a(dat, range / 2, mid + mid / 2);
 	// dat->f = sb_to_sa;
 	//qsort_b(dat, range / 2, mid - mid / 2);
 }
